01dd1c167bf87485e167973a324373daf577b22a
gvantsats <gcucxashvili@gmail.com>
2018-07-07 19:52:30 +0400

    countDown implemented

--- script.js
+++ script.js
@@ -15,7 +15,9 @@ var Countdown = {
 
         clearInterval(this.countdown_interval);
         clearInterval(this.firefly_interval);
-        $('.figure').css('visibility', 'visible');
+        $('#time').css('visibility', 'visible');
+        $('#period').css('display', 'none');
+
 
         // DOM
         this.$ = {
@@ -69,13 +71,10 @@ var Countdown = {
 
                 // Update DOM values
                 // Hours
-                that.checkHour(that.values.hours, $hour_1, $hour_2);
-
-                // Minutes
-                that.checkHour(that.values.minutes, $min_1, $min_2);
-
-                // Seconds
-                that.checkHour(that.values.seconds, $sec_1, $sec_2);
+                var h = that.values.hours > 9 ? that.values.hours : "0" + that.values.hours;
+                var m = that.values.minutes > 9 ? that.values.minutes : "0" + that.values.minutes;
+                var s = that.values.seconds > 9 ? that.values.seconds : "0" + that.values.seconds;
+                $("#time").html(h + ":" + m + ":" + s);
 
                 --that.total_seconds;
                 if (that.total_seconds < 60 && flag == 0) {
@@ -83,9 +82,9 @@ var Countdown = {
                     let firefly_togle = 0;
                     that.firefly_interval = setInterval(function () {
                         if (firefly_togle == 0) {
-                            $('.figure').css('visibility', 'hidden');
+                            $('#container').css('visibility', 'hidden');
                         } else {
-                            $('.figure').css('visibility', 'visible');
+                            $('#container').css('visibility', 'visible');
                         }
 
                         firefly_togle = 1 - firefly_togle;
@@ -95,78 +94,127 @@ var Countdown = {
             else {
                 clearInterval(that.countdown_interval);
                 clearInterval(that.firefly_interval);
-                $('.figure').css('visibility', 'visible');
+                $('#container').css('visibility', 'visible');
             }
         }, 1000);
     },
-
-    animateFigure: function ($el, value) {
-
-        var that = this,
-            $top = $el.find('.top'),
-            $bottom = $el.find('.bottom'),
-            $back_top = $el.find('.top-back'),
-            $back_bottom = $el.find('.bottom-back');
-
-        // Before we begin, change the back value
-        $back_top.find('span').html(value);
-
-        // Also change the back bottom value
-        $back_bottom.find('span').html(value);
-
-        // Then animate
-        TweenMax.to($top, 0.8, {
-            rotationX: '-180deg',
-            transformPerspective: 300,
-            ease: Quart.easeOut,
-            onComplete: function () {
-
-                $top.html(value);
-
-                $bottom.html(value);
-
-                TweenMax.set($top, { rotationX: 0 });
-            }
-        });
-
-        TweenMax.to($back_top, 0.8, {
-            rotationX: 0,
-            transformPerspective: 300,
-            ease: Quart.easeOut,
-            clearProps: 'all'
-        });
-    },
-
-    checkHour: function (value, $el_1, $el_2) {
-
-        var val_1 = value.toString().charAt(0),
-            val_2 = value.toString().charAt(1),
-            fig_1_value = $el_1.find('.top').html(),
-            fig_2_value = $el_2.find('.top').html();
-
-        if (value >= 10) {
-
-            // Animate only if the figure has changed
-            if (fig_1_value !== val_1) this.animateFigure($el_1, val_1);
-            if (fig_2_value !== val_2) this.animateFigure($el_2, val_2);
-        }
-        else {
-
-            // If we are under 10, replace first figure with 0
-            if (fig_1_value !== '0') this.animateFigure($el_1, 0);
-            if (fig_2_value !== val_1) this.animateFigure($el_2, val_1);
-        }
-    },
     
     setColor: function(color) {
-      $(".countdown .figure > span").css("color", color);
-      $(".top-back span").css("color", color);
+      $("#time").css("color", color);
     }
-    
-
 
 };
 
+// Create clock
+var Clock = {
+
+    // Backbone-like structure
+    $el: $('.countdown'),
+
+    // Params
+    countdown_interval: null,
+    firefly_interval: null,
+    total_seconds: 0,
+
+    // Initialize the countdown  
+    init: function (hour, minutes, seconds) {
+
+
+        clearInterval(this.countdown_interval);
+        clearInterval(this.firefly_interval);
+        $('#time').css('visibility', 'visible');
+        $('#period').css('display', 'block');
+
+
+        // DOM
+        this.$ = {
+            hours: this.$el.find('.bloc-time.hours .figure'),
+            minutes: this.$el.find('.bloc-time.min .figure'),
+            seconds: this.$el.find('.bloc-time.sec .figure')
+        };
+
+        // Init countdown values
+        this.values = {
+            hours: hour,
+            minutes: minutes,
+            seconds: seconds,
+        };
+
+        // Initialize total seconds
+        this.total_seconds = this.values.hours * 60 * 60 + (this.values.minutes * 60) + this.values.seconds;
+
+        // Animate countdown to the end 
+        this.count();
+    },
+
+    count: function () {
+
+        var that = this,
+            $hour_1 = this.$.hours.eq(0),
+            $hour_2 = this.$.hours.eq(1),
+            $min_1 = this.$.minutes.eq(0),
+            $min_2 = this.$.minutes.eq(1),
+            $sec_1 = this.$.seconds.eq(0),
+            $sec_2 = this.$.seconds.eq(1);
+        
+        let flag = 0;
+
+        this.countdown_interval = setInterval(function () {
+            if (that.total_seconds > 0) {
+
+                --that.values.seconds;
+
+                if (that.values.minutes >= 0 && that.values.seconds < 0) {
+
+                    that.values.seconds = 59;
+                    --that.values.minutes;
+                }
+
+                if (that.values.hours >= 0 && that.values.minutes < 0) {
+
+                    that.values.minutes = 59;
+                    --that.values.hours;
+                }
+
+                // Update DOM values
+                // Hours
+                var h = that.values.hours > 9 ? that.values.hours : "0" + that.values.hours;
+                var m = that.values.minutes > 9 ? that.values.minutes : "0" + that.values.minutes;
+                var s = that.values.seconds > 9 ? that.values.seconds : "0" + that.values.seconds;
+                $("#time").html(h + ":" + m + ":" + s);
+
+                --that.total_seconds;
+                if (that.total_seconds < 60 && flag == 0) {
+                    flag = 1;
+                    let firefly_togle = 0;
+                    that.firefly_interval = setInterval(function () {
+                        if (firefly_togle == 0) {
+                            $('#container').css('visibility', 'hidden');
+                        } else {
+                            $('#container').css('visibility', 'visible');
+                        }
+
+                        firefly_togle = 1 - firefly_togle;
+                    }, 250);
+                }
+            }
+            else {
+                clearInterval(that.countdown_interval);
+                clearInterval(that.firefly_interval);
+                $('#container').css('visibility', 'visible');
+            }
+        }, 1000);
+    },
+    
+    setColor: function(color) {
+      $("#time").css("color", color);
+    }
+
+};
+
+
+
+
 // Let's go !
-Countdown.init(0, 0, 0);
+Countdown.init(0, 1, 0);
 Countdown.setColor("#c3278f");
